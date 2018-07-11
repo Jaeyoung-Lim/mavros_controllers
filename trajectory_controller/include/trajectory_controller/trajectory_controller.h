859a6d46e4b50aa44c08a635108da4daac4273ac
JaeyoungLim <gokoreas@gmail.com>
2018-07-11 19:17:21 +0200

    Initial Build of package
    - Refactoring needed

---
+++ trajectory_controller/include/trajectory_controller/trajectory_controller.h
@@ -0,0 +1,110 @@
+//  July/2018, ETHZ, Jaeyoung Lim, jalim@student.ethz.ch
+
+#ifndef TRAJECTORY_CONTROLLER_H
+#define TRAJECTORY_CONTROLLER_H
+
+#include <ros/ros.h>
+#include <ros/subscribe_options.h>
+#include <kindr/Core>
+#include <tf/transform_broadcaster.h>
+#include <dynamic_reconfigure/server.h>
+
+#include <stdio.h>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+#include <Eigen/Dense>
+#include <mav_msgs/Actuators.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/AttitudeTarget.h>
+#include <std_srvs/SetBool.h>
+#include <gazebo_msgs/ModelStates.h>
+
+#define MODE_ROTORTHRUST  1
+#define MODE_BODYRATE     2
+#define MODE_BODYTORQUE   3
+
+using namespace std;
+using namespace Eigen;
+class trajectoryCtrl
+{
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_private_;
+    ros::Subscriber odomSub_;
+    ros::Subscriber referenceSub_;
+    ros::Subscriber keybrdSub_;
+    ros::Subscriber mavstateSub_;
+    ros::Subscriber mavposeSub_, gzmavposeSub_;
+    ros::Subscriber mavtwistSub_;
+    ros::Publisher rotorVelPub_, angularVelPub_;
+    ros::Publisher referencePosePub_;
+    ros::ServiceClient arming_client_;
+    ros::ServiceClient set_mode_client_;
+    ros::ServiceServer ctrltriggerServ_;
+    ros::Timer cmdloop_timer_, statusloop_timer_;
+    ros::Time last_request_, reference_request_now_, reference_request_last_;
+
+    int num_rotors_;
+    int max_motor_speed_;
+    bool fail_detec_, ctrl_enable_;
+    int ctrl_mode_;
+    bool landing_commanded_;
+    bool use_gzstates_, sim_enable_;
+    double kp_rot_, kd_rot_;
+    double reference_request_dt_;
+    double attctrl_tau_;
+    double norm_thrust_const_;
+    double max_fb_acc_;
+
+    mavros_msgs::State current_state_;
+    mavros_msgs::SetMode offb_set_mode_;
+    mavros_msgs::CommandBool arm_cmd_;
+    mavros_msgs::AttitudeTarget angularVelMsg_;
+    geometry_msgs::PoseStamped referencePoseMsg_;
+
+    Eigen::Vector3d goalPos_, targetPos_, targetVel_, targetPos_prev_, targetVel_prev_;
+    Eigen::Vector3d mavPos_, mavVel_, mavRate_;
+    double mavYaw_;
+    Eigen::Vector3d a_des, a_fb, a_ref, a_rd, g_;
+    Eigen::Vector4d mavAtt_, q_ref, q_des;
+    Eigen::Vector4d cmdBodyRate_; //{wx, wy, wz, Thrust}
+    Eigen::Vector3d Kpos_, Kvel_, D_;
+
+    void pubMotorCommands();
+    void pubRateCommands();
+    void pubReferencePose();
+    void odomCallback(const nav_msgs::OdometryConstPtr& odomMsg);
+    void targetCallback(const geometry_msgs::TwistStamped& msg);
+    void keyboardCallback(const geometry_msgs::Twist& msg);
+    void cmdloopCallback(const ros::TimerEvent& event);
+    void mavstateCallback(const mavros_msgs::State::ConstPtr& msg);
+    void mavposeCallback(const geometry_msgs::PoseStamped& msg);
+    void mavtwistCallback(const geometry_msgs::TwistStamped& msg);
+    void gzmavposeCallback(const gazebo_msgs::ModelStates& msg);
+    void statusloopCallback(const ros::TimerEvent& event);
+    bool ctrltriggerCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+    Eigen::Vector4d acc2quaternion(Eigen::Vector3d vector_acc, double yaw);
+    Eigen::Vector4d rot2Quaternion(Eigen::Matrix3d R);
+    Eigen::Matrix3d quat2RotMatrix(Eigen::Vector4d q);
+    geometry_msgs::PoseStamped vector3d2PoseStampedMsg(Eigen::Vector3d &position, Eigen::Vector4d &orientation);
+
+  public:
+    trajectoryCtrl(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+    void computeBodyRateCmd(bool ctrl_mode);
+    Eigen::Vector4d quatMultiplication(Eigen::Vector4d &q, Eigen::Vector4d &p);
+    Eigen::Vector4d attcontroller(Eigen::Vector4d &ref_att, Eigen::Vector3d &ref_acc, Eigen::Vector4d &curr_att);
+    virtual ~ trajectoryCtrl();
+};
+
+
+#endif
