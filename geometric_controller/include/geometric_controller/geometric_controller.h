5a8665c8addc50ef76a29f71eac7838dbfff1c70
JaeyoungLim <gokoreas@gmail.com>
2019-03-01 14:28:20 +0100

    Remove gazebo dependencies

--- geometric_controller/include/geometric_controller/geometric_controller.h
+++ geometric_controller/include/geometric_controller/geometric_controller.h
@@ -26,7 +26,6 @@
 #include <mavros_msgs/AttitudeTarget.h>
 #include <controller_msgs/FlatTarget.h>
 #include <std_srvs/SetBool.h>
-#include <gazebo_msgs/ModelStates.h>
 #include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
 #include <trajectory_msgs/MultiDOFJointTrajectory.h>
 
@@ -99,7 +98,6 @@ class geometricCtrl
     void mavstateCallback(const mavros_msgs::State::ConstPtr& msg);
     void mavposeCallback(const geometry_msgs::PoseStamped& msg);
     void mavtwistCallback(const geometry_msgs::TwistStamped& msg);
-    void gzmavposeCallback(const gazebo_msgs::ModelStates& msg);
     void statusloopCallback(const ros::TimerEvent& event);
     bool ctrltriggerCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
     bool landCallback(std_srvs::SetBool::Request& request, std_srvs::SetBool::Response& response);
