78f2ddcc5fc4330cddbb680ee51a28e1da24b18d
JaeyoungLim <gokoreas@gmail.com>
2019-04-13 13:20:16 +0200

    Add subscribers for states

--- controller_eval/include/controller_eval/controller_eval.h
+++ controller_eval/include/controller_eval/controller_eval.h
@@ -3,13 +3,37 @@
 #ifndef CONTROLLER_EVAL_H
 #define CONTROLLER_EVAL_H
 
+#include <Eigen/Dense>
+
 #include <ros/ros.h>
+#include <gazebo_msgs/ModelStates.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 
 class ControllerEvaluator
 {
   private:
     ros::NodeHandle nh_;
     ros::NodeHandle nh_private_;
+
+    ros::Subscriber mavposeSub_;
+    ros::Subscriber mavtwistSub_;
+    ros::Subscriber gzmavposeSub_;
+
+    std::string mav_name_;
+
+    Eigen::Vector3d mavPos_;
+    Eigen::Vector3d mavVel_;
+    Eigen::Vector3d mavRate_;
+    Eigen::Vector4d mavAtt_;
+    Eigen::Vector3d gt_mavPos_;
+    Eigen::Vector3d gt_mavVel_;
+    Eigen::Vector3d gt_mavRate_;
+    Eigen::Vector4d gt_mavAtt_;
+
+    void mavposeCallback(const geometry_msgs::PoseStamped& msg);
+    void mavtwistCallback(const geometry_msgs::TwistStamped& msg);
+    void gzmavposeCallback(const gazebo_msgs::ModelStates& msg);
 
   public:
     ControllerEvaluator(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
