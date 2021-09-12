23ebcbb124e03af5aa63561ef6da5613cd3fe870
Jaeyoung-Lim <jalim@ethz.ch>
2021-09-12 03:27:13 +0200

    Add trajectory lissajous
    F

--- Tools/run_controller.sh
+++ Tools/run_controller.sh
@@ -3,4 +3,4 @@
 set -e
 
 source $HOME/catkin_ws/devel/setup.bash
-roslaunch geometric_controller mav_trajectory_track_circle.launch
+roslaunch geometric_controller mav_trajectory_track_lissajous.launch
